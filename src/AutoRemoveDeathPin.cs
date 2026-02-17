d90f99f212c2b8198cd2ccda1b18ce5303db36ac
Yudi <madewahyudi22@gmail.com>
2026-02-17 13:35:44 +0800

    fix deathpin that is not removed on some cases

--- src/AutoRemoveDeathPin.cs
+++ src/AutoRemoveDeathPin.cs
@@ -1,6 +1,9 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using BepInEx;
+using BepInEx.Logging;
 using HarmonyLib;
+using UnityEngine;
 
 namespace AutoRemoveDeathPin
 {
@@ -10,6 +13,7 @@ namespace AutoRemoveDeathPin
         private const string Guid = "yudi7ll.autoremovedeathpin";
         private const string Name = "AutoRemoveDeathPin";
         private const string Version = "1.0.0";
+        public static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(Name);
 
         private Harmony _harmony;
     
@@ -18,14 +22,42 @@ namespace AutoRemoveDeathPin
             _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Guid);
         }
 
-        [HarmonyPatch(typeof(TombStone), "OnTakeAllSuccess")]
-        class Tombstone_OnTakeAllSuccess_Patch
+        [HarmonyPatch(typeof(ZNetView), "Destroy")]
+        class ZNetView_Destroy_Patch
         {
-            static void Postfix(TombStone __instance)
+            static void Prefix(ZNetView __instance)
             {
-                var deathPosition = __instance.transform.position;
-                const float radius = 0.1f;
-                Minimap.instance.RemovePin(deathPosition, radius);
+                if (!__instance) return;
+                var tombstone = __instance.GetComponent<TombStone>();
+                if (!tombstone) return;
+                Minimap.instance.RemovePin(tombstone.transform.position, 1.5f);
+            }
+        }
+        
+        
+        [HarmonyPatch(typeof(Player), "OnDeath")]
+        class Player_OnDeath_Patch
+        {
+            private static bool noTombstoneCreated = false;
+            
+            static void Prefix(Player __instance)
+            {
+                noTombstoneCreated = __instance.m_inventory.NrOfItems() == 0;
+            }
+            static void Postfix(Player __instance)
+            {
+                var pins = Minimap.instance.m_pins;
+                pins.Reverse();
+                
+                foreach (var pin in pins)
+                {
+                    if (pin.m_type != Minimap.PinType.Death) continue;
+                    if (!noTombstoneCreated) continue;
+                    
+                    Minimap.instance.RemovePin(pin);
+                    noTombstoneCreated = false;
+                    break;
+                }
             }
         }
 
